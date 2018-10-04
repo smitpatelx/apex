d6dfbc4f326bbcbc43ac660a8d57b7b27a77dda3
Clayton GP <claytongalliah@gmail.com>
2018-10-03 23:01:52 -0400

    clayton done

--- listing.php
+++ listing.php
@@ -15,7 +15,7 @@ require("./header.php");
 <script>
     var listings;
     var sql = "SELECT * FROM listing_demo ORDER BY listing_demo.id DESC";
-    $.get("/scripts/functions.php", {
+    $.get("./scripts/functions.php", {
         "sql": sql
     }, function($data) {
         listings = $data;
@@ -25,7 +25,7 @@ require("./header.php");
 
     var resultsPerPageArr = [8, 16, 32, -1];
     var resultsPerPage = resultsPerPageArr[0];
-    
+
     var page = 0;
     var offset = page * resultsPerPage;
 
@@ -37,9 +37,9 @@ require("./header.php");
 
         $("#results-per-page").change(function() {
             resultsPerPage = $("#results-per-page").val();
-            if (resultsPerPage == -1){
+            if (resultsPerPage == -1) {
                 resultsPerPage = listings.length;
-                
+
             }
             page = 0;
             console.log("Results per page: " + resultsPerPage);
@@ -65,41 +65,47 @@ require("./header.php");
         }
 
     }
+
     function PrintListing(div, i) {
-        var markup = "";
-        markup += "<div class='card cell z-depth-4 hoverable' data-aos='zoom-out'>";
-        markup += "<div class='card-image waves-effect waves-block waves-light'>";
-        markup += "<img class='activator' src='" + listings[i].img + "'>";
-        markup += "</div>";
-        markup += "<div class='card-content'>";
-        markup += "<span class='card-title activator grey-text text-darken-4'>" + listings[i].list_heading + "<i class='material-icons right'>more_vert</i></span>";
-        markup += "<p><a href='#' class='btn-flat waves-effect waves-block waves-light z-depth-4'>Know More</a></p>";
-        markup += "</div>";
-        markup += "<div class='card-reveal'>";
-        markup += "<span class='card-title grey-text text-darken-4 '>Information<i class='material-icons right'>close</i></span>";
-        markup += "<ul>";
-        markup += "<li>Location: " + listings[i].location + "</li>";
-        markup += "<li>Price: $" + listings[i].price + "</li>";
-        markup += "<li>Space: " + listings[i].sqft + " sqft</li>";
-        markup += "<li>Contact: " + listings[i].phone + "</li>";
-        markup += "</ul>";
-        markup += "</div>";
-        markup += "</div>";
-        div.append(markup);
+        try {
+            var markup = "";
+            markup += "<div class='card cell z-depth-4 hoverable' data-aos='zoom-out'>";
+            markup += "<div class='card-image waves-effect waves-block waves-light'>";
+            markup += "<img class='activator' src='" + listings[i].img + "'>";
+            markup += "</div>";
+            markup += "<div class='card-content'>";
+            markup += "<span class='card-title activator grey-text text-darken-4'>" + listings[i].list_heading + "<i class='material-icons right'>more_vert</i></span>";
+            markup += "<p><a href='#' class='btn-flat waves-effect waves-block waves-light z-depth-4'>Know More</a></p>";
+            markup += "</div>";
+            markup += "<div class='card-reveal'>";
+            markup += "<span class='card-title grey-text text-darken-4 '>Information<i class='material-icons right'>close</i></span>";
+            markup += "<ul>";
+            markup += "<li>Location: " + listings[i].location + "</li>";
+            markup += "<li>Price: $" + listings[i].price + "</li>";
+            markup += "<li>Space: " + listings[i].sqft + " sqft</li>";
+            markup += "<li>Contact: " + listings[i].phone + "</li>";
+            markup += "</ul>";
+            markup += "</div>";
+            markup += "</div>";
+            div.append(markup);
+        } catch (error) {
+
+        }
+
     }
 
     function pageChange(value) {
-        
+
         if (page != -1) {
             page += value;
             if (page == -1) {
                 page = 0;
-            } else if ((page+1)*resultsPerPage > listings.length) {
-                
+            } else if (page * resultsPerPage > listings.length) {
+
             } else {
-            console.log("page changed to " + page);
-            offset = page * resultsPerPage;
-            PopulateListings();
+                console.log("page changed to " + page);
+                offset = page * resultsPerPage;
+                PopulateListings();
             }
         }
     }
@@ -126,25 +132,25 @@ require("./header.php");
         </select>
     </div>
 </div>
-<div class="grid-x grid-margin-x ml-2 pb-2">
+<div class="grid-x grid-margin-x pb-2">
     <div class="cell large-1 large-offset-5">
-        <a class="btn-small" onclick="pageChange(-1)"><i class="fas fa-arrow-left"></i></a>
+        <a class="btn-small" onclick="pageChange(-1)" style="float: right;"><i class="fas fa-arrow-left"></i></a>
     </div>
     <div class="cell large-1">
         <a class="btn-small" onclick="pageChange(1)"><i class="fas fa-arrow-right"></i></a>
     </div>
 </div>
 
-<div class="grid-x grid-padding-x ml-3">
+<div class="grid-x grid-padding-x" style="justify-content: center;">
     <div id="listing-output" class="cell medium-12 large-12 small-12 grid-x grid-margin-x small-up-2 medium-up-2 large-up-4">
 
 
     </div>
 </div>
 
-<div class="grid-x grid-margin-x ml-2 pb-2">
+<div class="grid-x grid-margin-x pb-2">
     <div class="cell large-1 large-offset-5">
-        <a class="btn-small" onclick="pageChange(-1)"><i class="fas fa-arrow-left"></i></a>
+        <a class="btn-small" onclick="pageChange(-1)" style="float: right;"><i class="fas fa-arrow-left"></i></a>
     </div>
     <div class="cell large-1">
         <a class="btn-small" onclick="pageChange(1)"><i class="fas fa-arrow-right"></i></a>
